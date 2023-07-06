6803a315180bf3f69327e951b328ed0931bb6dee
roamingterra <danielballerini1@gmail.com>
2023-07-06 11:37:53 -0400

    Finish project
    Unfortunately, I did not create any other commits throughout the
    creation of this project. It is a small project however, so not much was
    needed. The idea was to create several html pages, and a node server to
    serve these pages based on the appropriate url. I also added some
    JavaScript to direct the user more easily to the other html pages by
    clicking on the various icons and buttons.

---
+++ public/scripts/script.js
@@ -0,0 +1,13 @@
+// Client-side functions that modify the browser location,
+// which triggers the browser to make a request to navigate to the specified url
+function loadHomePage() {
+  window.location.href = "/";
+}
+
+function loadAboutPage() {
+  window.location.href = "/about.html";
+}
+
+function loadContactPage() {
+  window.location.href = "/contact-me.html";
+}
